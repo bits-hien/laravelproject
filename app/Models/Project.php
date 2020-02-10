b7e09a59ae150e51a232d979507e43280dcec3b0
bits-hien <hienthinguyenvp@gmail.com>
2020-02-10 16:19:53 +0700

    change1-4

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -7,26 +7,26 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     protected $fillable = [
-    	'name', 'start_time', 'finish_time', 'customer_id', 'leader_id', 'status_id', 'description'
+        'name', 'start_time', 'finish_time', 'customer_id', 'leader_id', 'status_id', 'description'
     ];
 
     public function members()
     {
-    	return $this->belongsToMany(Member::class);
+        return $this->belongsToMany(Member::class);
     }
 
     public function customer()
     {
-    	return $this->hasOne(Customer::class);
+        return $this->hasOne(Customer::class);
     }
 
     public function status()
     {
-    	return $this->hasOne(Status::class);
+        return $this->hasOne(Status::class);
     }
 
     public function tasks()
     {
-    	return $this->hasMany(Task::class);
+        return $this->hasMany(Task::class);
     }
 }
